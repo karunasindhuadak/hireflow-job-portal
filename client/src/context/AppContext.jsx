07b7cacd4b38fc69e78c0cc1c7165e9413e8a950
Karunasindhu Adak <karunasindhuadak7@gmail.com>
2026-04-08 21:14:33 +0530

    feat: add job fetching functionality to AppContext

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,4 +1,5 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
+import { jobsData } from "../assets/assets";
 
 export const AppContext = createContext();
 
@@ -8,11 +9,21 @@ export const AppContextProvider = (props) => {
     location: "",
   });
   const [isSearched, setIsSearched] = useState(false);
+  const [jobs, setJobs] = useState([]);
+
+  const fetchJobs = async () => {
+    setJobs(jobsData);
+  };
+  useEffect(() => {
+    fetchJobs();
+  }, []);
   const value = {
     searchFilter,
     setSearchFilter,
     isSearched,
     setIsSearched,
+    jobs,
+    setJobs,
   };
   return (
     <AppContext.Provider value={value}>{props.children}</AppContext.Provider>
